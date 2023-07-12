c491640e83c1a07d9aefa0212c9ac9c639bc3263
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-12 10:31:01 +0530

    creting view enjine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = (req, res) =>{
-    return res.end('<h1>Express is up for Major Project.</h1>')
+    // return res.end('<h1>Express is up for Major Project.</h1>')
+    return res.render('home', {
+        title: "Home"
+    });
 }
