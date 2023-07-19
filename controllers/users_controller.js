2f0e3436626a6a1e2abe87ba4b21c6270355dbba
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-19 11:36:41 +0530

    Created User View Page Sign-In and Sign-Out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,18 @@ module.exports.profile = (req, res) => {
         title: "User Profile"
     });
 }
+
+// Render the Sign Up Page
+module.exports.signUp = (req, res) =>{
+    return res.render('user_sign_up',{
+        title : "Major Project | Sign Up"
+    })
+}
+
+
+// Render the Sign In Page
+module.exports.signIn = (req, res) =>{
+    return res.render('user_sign_in', {
+        title : "Major Project | Sign In"
+    })
+}
