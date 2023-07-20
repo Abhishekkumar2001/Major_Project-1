f4881a278d2de16a3dafb7ce696c1f132061e40a
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-20 10:30:03 +0530

    Sign Up completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 module.exports.profile = (req, res) => {
     return res.render('user_profile', {
         title: "User Profile"
@@ -17,4 +18,48 @@ module.exports.signIn = (req, res) =>{
     return res.render('user_sign_in', {
         title : "Major Project | Sign In"
     })
+}
+
+// get the sign up data
+// module.exports.create = (req, res) =>{
+//     if(req.body.password != req.body.confirm_password){
+//         return res.redirect('back');
+//     }
+//     User.findOne({ email : req.body.email}, (err, user) =>{
+//         if(err){console.log('Error in Finding user in signing up'); return}
+
+//         if(!user){
+//             User.create(req.body, (err, user) =>{
+//                 if(err){console.log('Error in creating user while signing in')};
+
+//                 return res.redirect('/users/sign-in');
+//             })
+//         }else{
+//             return res.redirect('back');
+//         }
+//     })
+// }
+module.exports.create = async (req, res) => {
+    try {
+        if (req.body.password != req.body.confirm_password) {
+            return res.redirect('back');
+        }
+
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log('Error:', err.message);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+//Sign In and Create a session for the user
+module.exports.createSession = (req, res) => {
+    //TODO later
 }
