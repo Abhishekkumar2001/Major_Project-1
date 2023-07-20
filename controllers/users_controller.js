b5ca42108709f329c4e5723084dddee16e98cbe0
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-20 14:25:07 +0530

    Show Details of Signed in User

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,24 @@
 const User = require('../models/user');
-module.exports.profile = (req, res) => {
-    return res.render('user_profile', {
-        title: "User Profile"
-    });
+
+// Render the Profile Page
+module.exports.profile = async (req, res) => {
+    try {
+        const user = await User.findById(req.cookies.user_id);
+        if (user) {
+            res.render('user_profile', {
+                title: "User Profile",
+                user: user
+            });
+        } else {
+            return res.redirect('/users/sign-in');
+        }
+
+    }
+    catch (err) {
+        console.log('Error:', err.message);
+        return res.status(500).send('Internal Server Error');
+    }
+
 }
 
 // Render the Sign Up Page
@@ -21,24 +37,6 @@ module.exports.signIn = (req, res) => {
 }
 
 // get the sign up data
-// module.exports.create = (req, res) =>{
-//     if(req.body.password != req.body.confirm_password){
-//         return res.redirect('back');
-//     }
-//     User.findOne({ email : req.body.email}, (err, user) =>{
-//         if(err){console.log('Error in Finding user in signing up'); return}
-
-//         if(!user){
-//             User.create(req.body, (err, user) =>{
-//                 if(err){console.log('Error in creating user while signing in')};
-
-//                 return res.redirect('/users/sign-in');
-//             })
-//         }else{
-//             return res.redirect('back');
-//         }
-//     })
-// }
 module.exports.create = async (req, res) => {
     try {
         if (req.body.password != req.body.confirm_password) {
@@ -67,16 +65,17 @@ module.exports.createSession = async (req, res) => {
         // find the user
         const user = await User.findOne({ email: req.body.email });
 
+        //handle user found
         if (user) {
-            //handle user found
-            if (user.password != req.body.password){
+            // handle password which doesn't match
+            if (user.password != req.body.password) {
                 return res.redirect('back');
             }
 
-            //handle password which don't match
+            //handle session creation
             res.cookie('user_id', user.id);
             return res.redirect('/users/profile');
-            
+
         } else {
             //handle user not found
             return res.redirect('back');
@@ -86,12 +85,6 @@ module.exports.createSession = async (req, res) => {
         console.log('Error:', err.message);
         return res.status(500).send('Internal Server Error');
     }
-
-
-
-
-    //handle session creation
-
 
 
 }
