cd13042d230d9ff0c6c265fb938c87b2a3c93efb
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-21 10:40:39 +0530

    Setting Up Passport Js

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -23,6 +23,28 @@ passport.use(new LocalStrategy({
         }
         return done(null, user); 
     });
+    // Serializing the user to decide which key is be kept in the cookies
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+
+        //deserializing the user from the key in the cookies
+
+        passport.deserializeUser((id, done) =>{
+            User.findById(id, (err, user)=>{
+                if(err){
+                    console.log('Error in finding user --> Passport');
+                    return done(err);
+                }
+                return done(null, user);
+            });
+        })
+
+    });
 }
 
 ));
+
+
+
+
+module.exports = passport;
