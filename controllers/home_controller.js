30a0206c506c1974cff7ffd994166dae8fbcf0ca
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-08-02 11:36:57 +0530

    Converting to Async and Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,34 +1,27 @@
 const Post = require("../models/post");
-const User = require('../models/user');
+const User = require("../models/user");
 
 // populate the user of each post
-module.exports.home = (req, res) => {
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: 'comments',
-      populate: {
-        path: 'user'
-      }
-    })
-    .exec((err, posts) => {
-
-      User.find({}, (err, users)=>{
-        return res.render("home", {
-          title: "Major Project | Home",
-          posts: posts,
-          all_users: users
-        });
+module.exports.home = async (req, res) => {
+  try {
+    // populate the user of each post
+    const posts = await Post.find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
 
-     
+    const users = await User.find({});
+    return res.render("home", {
+      title: "Major Project | Home",
+      posts: posts,
+      all_users: users,
     });
-
-  // Post.find({}, (err, posts)=> {
-  //     return res.render('home', {
-  //         title: "Major Project | Home",
-  //         posts: posts
-  //     });
-  // });
-  // return res.end('<h1>Express is up for Major Project.</h1>')
+  } catch (err) {
+    console.log('Error',err);
+    return;
+  }
 };
