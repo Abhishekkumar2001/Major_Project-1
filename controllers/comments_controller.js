f83acd40ee241939d34d4b02392d85414ec6f02d
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-08-11 10:51:13 +0530

    Sending our first email via smtp

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,43 +1,86 @@
-const Comment = require("../models/comment");
-const Post = require("../models/post");
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+module.exports.create = async function(req, res){
 
-module.exports.create = async (req, res) => {
-  try {
-    const post = await Post.findById(req.body.post);
-    if (post) {
-      const comment = await Comment.create({
-        content: req.body.content,
-        post: req.body.post,
-        user: req.user._id,
-      });
-      post.comments.push(comment);
-      post.save();
+    try{
+        let post = await Post.findById(req.body.post);
 
-      res.redirect("/");
+        if (post){
+            let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+
+            
+            post.comments.push(comment);
+            post.save();
+            
+            comment = await comment.populate('user', 'name email')
+            commentsMailer.newComment(comment);
+            
+            if (req.xhr){
+              //comment = await comment.populate('user', 'name email')
+            
+                return res.status(200).json({
+                    data: {
+                        comment: comment,
+                        username: req.user.name
+                    },
+                    message: "Comment created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
+        }
+    }catch(err){
+        req.flash('error', err);
+        console.log(err);
+        return;
     }
-  } catch (err) {
-    console.log(`error in creating a post ${err}`);
-    return;
-  }
-};
+    
+}
 
-module.exports.destroy = async (req, res) => {
-  try {
-    const comment = await Comment.findById(req.params.id);
-    if (comment.user == req.user.id) {
-      let postId = comment.post;
 
-      comment.remove();
+module.exports.destroy = async function(req, res){
 
-      let post = await Post.findByIdAndUpdate(postId, {
-        $pull: { comments: req.params.id },
-      });
-      return res.redirect("back");
-    } else {
-      return res.redirect("back");
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'Unauthorized');
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log(err);
+        req.flash('error', err);
+        return;
     }
-  } catch (err) {
-    console.log("Error", err);
-    return;
-  }
-};
+    
+}
---
+++ mailers/comments_mailer.js
@@ -0,0 +1,44 @@
+const nodeMailer = require("../config/nodemailer");
+
+exports.newComment = (comment) => {
+  console.log("inside newComment mailer", comment);
+
+  nodeMailer.transporter.sendMail(
+    {
+      from: "abhikum8131@gmail.com",
+      to: comment.user.email,
+      subject: "New Comment Published!",
+      html: "<h1>Yup, your comment is now published!</h1>",
+    },
+    (err, info) => {
+      if (err) {
+        console.log("Error in sending mail", err);
+        return;
+      }
+
+      console.log("Message sent", info);
+      return;
+    }
+  );
+};
+
+// async function sendMail(){
+
+//   // create an email transporter
+
+//   const mailOptions = {
+//     from: 'abhikum8131@gmail.com',
+//     to: 'comment.user.email',
+//     subject: 'Welcome to NodeJS App',
+//     text: 'This is an email using nodemailer in NodeJS',
+// };
+
+// // 3. Send the email
+// try{
+//     const result = await nodeMailer.transporter.sendMail(mailOptions);
+//     console.log("Email sent successfully");
+// }catch(err){
+//     console.log('Email send failer with error: '+ err);
+// }
+// }
+// sendMail();
